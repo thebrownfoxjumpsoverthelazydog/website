2829eaee9ffb16486398cda8fd35879870f1468b
thebrownfoxjumpsoverthelazydog <167606804+thebrownfoxjumpsoverthelazydog@users.noreply.github.com>
2024-04-24 23:10:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Array of image URLs (replace these with your own image URLs)
     const imageUrls = [
-        'https://discourse.disneyheroesgame.com/uploads/default/original/3X/f/6/f6c0444a65bb8ea035d671ec8d4acdce982aae39.jpeg',
-        'https://us-tuna-sounds-images.voicemod.net/decbf6b1-5b72-42ee-865d-5534b31f99ad-1672568939460.jpg',
+        'https://www.nrgpark.com/wp-content/uploads/HCC_1920_1080.jpg',
+        'https://clipground.com/images/u-of-h-logo-3.png',
         'https://pm1.narvii.com/6298/9624c39ba4925b63480ca937ffacf2b36785d425_hq.jpg'
         // Add more image URLs as needed
     ];
